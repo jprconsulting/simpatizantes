a9e48f91f95b9dacb4ad7fc38dcbb10f31dd3446
Ruby Montes <ruby.montes.rosas@gmail.com>
2023-11-30 16:43:39 -0600

    biembenida

--- src/shared/components/cards/card-line-chart/card-line-chart.component.ts
+++ src/shared/components/cards/card-line-chart/card-line-chart.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, AfterViewInit } from "@angular/core";
 @Component({
   selector: "app-card-line-chart",
   templateUrl: "./card-line-chart.component.html",
+  styleUrls: ['./card-line-chart.component.css']
 })
 export class CardLineChartComponent implements OnInit {
   constructor() { }
