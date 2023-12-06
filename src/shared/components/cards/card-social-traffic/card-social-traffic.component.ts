f50eb7f5df5343f2034f40cc1bb71e245ed2513d
Gabriel <g27r02@gmail.com>
2023-12-06 16:43:04 -0600

    Beneficiaros

--- src/shared/components/cards/card-social-traffic/card-social-traffic.component.ts
+++ src/shared/components/cards/card-social-traffic/card-social-traffic.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { BeneficiarioService } from "src/app/pages/core/services/Beneficiario.service";
 import { MunicipiosService } from "src/app/pages/core/services/municipios.service";
+import { Beneficiario } from "src/app/pages/models/beneficiario";
 import { Municipios } from "src/app/pages/models/municipios";
 declare const google: any;
 @Component({
@@ -8,9 +10,15 @@ declare const google: any;
 })
 export class CardSocialTrafficComponent{
   municipios: Municipios[] = [];
+  beneficiarios: Beneficiario[] = [];
+
   municipiosOptions: { nombre: string, label: string }[] = [];
-  constructor(private municipiosService: MunicipiosService,) {}
+  constructor(
+    private municipiosService: MunicipiosService,
+    private beneficiarioService: BeneficiarioService,
+    ) {}
   showModal = false;
+
 
   openModal(): void {
     this.showModal = true;
@@ -21,15 +29,28 @@ export class CardSocialTrafficComponent{
   }
   ngOnInit() {
     this.obtenerMunicipios();
+    this.obtenerBeneficiarios();
   }
-  
+
+  obtenerBeneficiarios() {
+    this.beneficiarioService.getBeneficiario().subscribe(
+      (beneficiarios: Beneficiario[]) => {
+        console.log('Datos de beneficiarios recibidos:', beneficiarios);
+        this.beneficiarios = beneficiarios;
+      },
+      (error: any) => {
+        console.error('Error al obtener beneficiarios:', error);
+      }
+    );
+  }
+
   obtenerMunicipios() {
     if (this.municipiosService) {
       this.municipiosService.getMunicipios().subscribe(
         (municipios: Municipios[]) => {
           console.log('Datos de municipios recibidos:', municipios);
           this.municipios = municipios;
-  
+
           // Actualizar las opciones del select
           this.municipiosOptions = municipios.map(municipio => ({
             nombre: municipio.id.toString(),  // Ajusta el valor según la estructura de tus municipios
