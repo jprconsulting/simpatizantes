2d8bb713e55b1a8ceb6964cf12e75cc4e0ad8651
ximena-armaas <114454320+ximena-armaas@users.noreply.github.com>
2023-12-13 17:01:02 -0600

    nube de palabras

--- src/app/pages/panel/nube-palabras/nube-palabras.component.ts
+++ src/app/pages/panel/nube-palabras/nube-palabras.component.ts
@@ -1,73 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit,AfterViewInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 import * as Highcharts from 'highcharts';
+
+declare var require: any;
+const More = require('highcharts/highcharts-more');
+More(Highcharts);
+
+import Histogram from 'highcharts/modules/histogram-bellcurve';
+Histogram(Highcharts);
+
+const Accessibility = require('highcharts/modules/accessibility');
+Accessibility(Highcharts);
+
+const Wordcloud = require('highcharts/modules/wordcloud');
+Wordcloud(Highcharts);
+
+
 
 @Component({
   selector: 'app-nube-palabras',
   templateUrl: './nube-palabras.component.html',
-  styleUrls: ['./nube-palabras.component.css']
+  styleUrls: ['./nube-palabras.component.css'],
+  styles: ['canvas { width: 100%; height: 400px; }']
 })
+
 export class NubePalabrasComponent implements OnInit {
-  ngOnInit(): void {
-    const data = [{
-      name: 'creativity',
-      weight: 31
-  }, {
-      name: 'creative',
-      weight: 22
-  }, {
-      name: 'intelligence',
-      weight: 15
-  }, {
-      name: 'more',
-      weight: 12
-  }, {
-      name: 'people',
-      weight: 12
-  }, {
-      name: 'theory',
-      weight: 11
-  }, {
-      name: 'problem',
-      weight: 11
-  }, {
-      name: 'thinking',
-      weight: 11
-  }, {
-      name: 'been',
-      weight: 11
-  }, {
-      name: 'can',
-      weight: 11
-  }, {
-      name: 'process',
-      weight: 11
-  }, {
-      name: 'new',
-      weight: 10
-  },];
+  public activity: any;
+  public xData: any;
+  public label: any;
+  options: any;
 
-      Highcharts.chart('container', {
+  constructor() {
+    let arr = [
+      'app',
+      'html',
+      'component',
+      'testing',
+      'whatsapp',
+      'users',
+      'code',
+      'feedback',
+      'service',
+      'angular',
+      'option',
+      'motivated',
+    ];
+    let val = [
+      46, 34, 33, 28, 26, 24, 23, 17, 17, 16, 12, 12, 12, 12, 12, 12, 12,
+      11, 11, 11, 11, 11, 11, 11, 9, 9, 9, 9, 8,
+    ];
 
-        series: [{
-            colors: ['#FFAAAA', '#D46A6A', '#AA3939', '#801515', '#550000'],
-            rotation: {
-                from: -60,
-                to: 60,
-                orientations: 5
-            },
-            type: 'wordcloud',
-            data: data
-        }],
-        title: {
-            text: 'Most popular words used to describe creativity'
+    let data = arr.map((value, index) => {
+      return { name: value, weight: val[index] };
+    });
+
+    this.options = {
+      accessibility: {
+        screenReaderSection: {
+          beforeChartFormat:
+            '<h5>{chartTitle}</h5>' +
+            '<div>{chartSubtitle}</div>' +
+            '<div>{chartLongdesc}</div>' +
+            '<div>{viewTableButton}</div>',
         },
-        tooltip: {
-            enabled: false
+      },
+      series: [
+        {
+          colors: ['#FFAAAA', '#D46A6A', '#AA3939', '#801515', '#550000'],
+          type: 'wordcloud',
+          data: data,
+          name: 'palabra',
+          minFontSize: 15,
+          rotation: {
+            from: -60,
+            to: 60,
+            orientations: 5
+          },
         },
-        subtitle: {
-            text: '<a href="https://en.wikipedia.org/wiki/Creativity">Source: Wikipedia</a>'
-        }
-      });
+      ],
+      title: {
+        text: '',
+      },
+    };
   }
 
+  ngOnInit() {
+    Highcharts.chart('container', this.options);
+  }
 }
+
