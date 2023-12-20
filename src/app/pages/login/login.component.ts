3ac0bcf37b426b681dae0b7694c021c73508ad0d
unknown <juarez_s3@hotmail.com>
2023-12-20 00:39:15 -0600

    :sparkles: login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AppUser } from '../models/login';
+import { SecurityService } from '../core/services/security.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { MensajeService } from '../core/services/mensaje.service';
 
 
 @Component({
@@ -7,12 +12,51 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  constructor(private router: Router) {
+export class LoginComponent implements OnInit {
+  formUserLogin!: FormGroup;
+  user!: AppUser;
+  returnUrl = 'panel/inicio';
 
-  }
-  redireccionarAOtraPagina() {
-    this.router.navigate(['/panel']);
+
+  constructor(
+    private securityService: SecurityService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private spinnerService: NgxSpinnerService,
+    private mensajeService: MensajeService,
+
+  ) { }
+
+  ngOnInit(): void {
+    this.createFormUserLogin();
   }
 
+  login() {
+    this.user = this.formUserLogin.value as AppUser;
+    this.spinnerService.show();
+    this.securityService.login(this.user).subscribe({
+      next: () => {
+        setTimeout(() => {
+          this.spinnerService.hide();
+          this.router.navigateByUrl(this.returnUrl);
+        }, 500);
+      },
+      error: (error: string) => {
+        this.mensajeService.mensajeError(error);
+        this.spinnerService.hide();
+      }
+    });
+  }
+
+
+  createFormUserLogin() {
+    this.formUserLogin = this.formBuilder.group({
+      email: ['', Validators.compose([
+        Validators.required, Validators.minLength(3)
+      ])],
+      password: ['', Validators.compose([
+        Validators.required, Validators.minLength(3)
+      ])]
+    });
+  }
 }
